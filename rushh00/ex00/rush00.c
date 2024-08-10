2bddfd879e6bc530c9a1bbf212d42d9af6ac5a51
Nicolas Lambert <niclambe@c1r6p17.42lehavre.fr>
2024-08-10 13:37:03 +0200

    rush00 fini

--- rushh00/ex00/rush00.c
+++ rushh00/ex00/rush00.c
@@ -6,75 +6,81 @@
 /*   By: niclambe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/10 11:27:56 by niclambe          #+#    #+#             */
-/*   Updated: 2024/08/10 12:20:48 by niclambe         ###   ########.fr       */
+/*   Updated: 2024/08/10 13:23:02 by niclambe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_putchar(char c)
+void	ft_putchar(char c);
+
+void	hortop(int x)
 {
-	write(1, &c, 1);
+	int	hor;
+
+	hor = 0;
+	while (x > hor)
+	{
+		if (hor == 0 || hor == x - 1)
+			ft_putchar('o');
+		else
+			ft_putchar('-');
+		hor++;
+	}
+	ft_putchar('\n');
+}
+
+void	vermid(int x, int y)
+{
+	int	ver;
+	int	hor;
+
+	ver = 1;
+	while (ver < y - 1)
+	{
+		ft_putchar('|');
+		hor = 1;
+		while (hor < x - 1)
+		{
+			ft_putchar(' ');
+			hor++;
+		}
+		if (x > 1)
+			ft_putchar('|');
+		ft_putchar('\n');
+		ver++;
+	}
+}
+
+void	horbot(int x, int y)
+{
+	int	hor;
+
+	if (y > 1)
+	{
+		hor = 0;
+		while (hor < x)
+		{
+			if (hor == 0 || hor == x - 1)
+				ft_putchar('o');
+			else
+				ft_putchar('-');
+			hor++;
+		}
+		ft_putchar('\n');
+	}
 }
 
 void	rush(int x, int y)
 {
-	int	hor;
-	int	ver;
-
 	if (x != 0 && y != 0)
 	{
-		hor = 0;
-		while (x > hor)
-		{
-			if (hor == 0 || hor == x - 1)
-			{
-				ft_putchar('o');
-			}
-			else
-			{
-				ft_putchar('-');
-			}
-			hor++;
-		}
-		ft_putchar('\n');
-		ver = 1;
-		while (ver < y - 1)
-		{
-			ft_putchar('|');
-			hor = 1;
-			while (hor < x - 1)
-			{
-				ft_putchar(' ');
-				hor++;
-			}
-			if (x > 1)
-				ft_putchar('|');
-			ft_putchar('\n');
-			ver++;
-		}
-		if (y > 1)
-		{
-			hor = 0;
-			while (hor < x)
-			{
-				if (hor == 0 || hor == x - 1)
-					ft_putchar('o');
-				else
-					ft_putchar('-');
-				hor++;
-			}
-			ft_putchar('\n');
-		}
+		hortop(x);
+		vermid(x, y);
+		horbot(x, y);
 	}
 	else
 	{
 		ft_putchar('\0');
 	}
-}
-
-int	main(void)
-{
-	rush(0, 0);
-	return (0);
 }
