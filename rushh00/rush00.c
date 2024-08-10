837e533562025230a994ce4f11858fd00db28e53
Nicolas Lambert <niclambe@c1r6p17.42lehavre.fr>
2024-08-10 11:24:37 +0200

    update

--- rushh00/rush00.c
+++ rushh00/rush00.c
@@ -7,15 +7,59 @@ void ft_putchar(char c)
 
 void    rush(int x, int y)
 {
-    while (x > 0)
-    {
-        ft_putchar('-');
-    }
+    int hor;
+    int ver;
 
+    hor = 0;
+    //boucle horizontal
+    while (x > hor)
+    {
+        if (hor == 0 || hor == x-1)
+        {
+            ft_putchar('o');
+        }
+        else
+        {
+            ft_putchar('-');
+        }
+        hor++;
+    }
+    ft_putchar('\n');
+
+    //boucle vertical
+    ver = 1;
+    while (ver < y - 1)
+    {
+        ft_putchar('|');
+        hor = 1;
+        while (hor < x - 1)
+        {
+            ft_putchar(' ');
+            hor++;
+        }
+        if (x > 1)
+            ft_putchar('|');
+        ft_putchar('\n');
+        ver++;
+    }
+    //boucle horizontal du bas
+    if (y > 1)
+    {
+        hor = 0;
+        while (hor < x)
+        {
+            if (hor == 0 || hor == x - 1)
+                ft_putchar('o');
+            else
+                ft_putchar('-');
+            hor++;
+        }
+        ft_putchar('\n');
+    }
 }
 
 int main(void)
 {
-    rush(3, 0);
+    rush(5, 5);
     return (0);
 }
