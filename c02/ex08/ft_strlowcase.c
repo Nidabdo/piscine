0e6db09662b823d294d04e262444d8d365e2f02d
Nicolas Lambert <niclambe@c1r6p17.42lehavre.fr>
2024-08-11 13:36:48 +0200

    update

--- c02/ex08/ft_strlowcase.c
+++ c02/ex08/ft_strlowcase.c
@@ -1,32 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strupcase.c                                     :+:      :+:    :+:   */
+/*   ft_strlowcase.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: niclambe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/11 12:57:15 by niclambe          #+#    #+#             */
-/*   Updated: 2024/08/11 13:24:25 by niclambe         ###   ########.fr       */
+/*   Created: 2024/08/11 13:30:53 by niclambe          #+#    #+#             */
+/*   Updated: 2024/08/11 13:34:50 by niclambe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 //#include <stdio.h>
 
-char	*ft_strupcase(char *str)
+char	*ft_strlowcase(char *str)
 {
 	int	i;
 
 	i = 0;
 	while (str[i] != '\0')
 	{
-		if (str[i] >= 'a' && str[i] <= 'z')
+		if (str[i] >= 'A' && str[i] <= 'Z')
 		{
-			str[i] = str[i] - 32;
+			str[i] = str[i] + 32;
 		}
 		i++;
 	}
 	return (str);
 }
+
 /*
 int main(void)
 {
@@ -37,19 +38,19 @@ int main(void)
 	char str5[] = "";
 
 	printf("Original: %s\n", str1);
-	printf("Uppercase: %s\n\n", ft_strupcase(str1));
+	printf("Uppercase: %s\n\n", ft_strlowcase(str1));
 
 	printf("Original: %s\n", str2);
-	printf("Uppercase: %s\n\n", ft_strupcase(str2));
+	printf("Uppercase: %s\n\n", ft_strlowcase(str2));
 
 	printf("Original: %s\n", str3);
-	printf("Uppercase: %s\n\n", ft_strupcase(str3));
+	printf("Uppercase: %s\n\n", ft_strlowcase(str3));
 
 	printf("Original: %s\n", str4);
-	printf("Uppercase: %s\n\n", ft_strupcase(str4));
+	printf("Uppercase: %s\n\n", ft_strlowcase(str4));
 
 	printf("Original: \"%s\"\n", str5);
-	printf("Uppercase: \"%s\"\n", ft_strupcase(str5));
+	printf("Uppercase: \"%s\"\n", ft_strlowcase(str5));
 
 	return 0;
 }
