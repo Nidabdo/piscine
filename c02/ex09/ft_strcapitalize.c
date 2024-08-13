017c073d0baefccc7963589b4ebdfad26b352f32
Nicolas Lambert <niclambe@c1r5p8.42lehavre.fr>
2024-08-13 17:01:30 +0200

    update

--- c02/ex09/ft_strcapitalize.c
+++ c02/ex09/ft_strcapitalize.c
@@ -6,11 +6,11 @@
 /*   By: niclambe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/11 13:38:44 by niclambe          #+#    #+#             */
-/*   Updated: 2024/08/12 08:07:42 by niclambe         ###   ########.fr       */
+/*   Updated: 2024/08/13 15:36:30 by niclambe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include <stdio.h>
+/* #include <stdio.h> */
 
 char	*ft_strcapitalize(char *str)
 {
@@ -18,11 +18,11 @@ char	*ft_strcapitalize(char *str)
 
 	i = 1;
 	if (str[0] >= 'a' && str[0] <= 'z')
-	{
 		str[0] -= 32;
-	}
 	while (str[i] != '\0')
 	{
+		while (str[i] >= 'A' && str[i] <= 'Z')
+			str[i] += 32;
 		if (!(str[i - 1] >= '0' && str[i - 1] <= '9'))
 		{
 			if (!(str[i - 1] >= 'a' && str[i - 1] <= 'z'))
@@ -38,11 +38,11 @@ char	*ft_strcapitalize(char *str)
 	}
 	return (str);
 }
-/*
-int main(void)
+
+/* int main(void)
 {
-	char str[] = "salut, comment tu vas ? 42mots quarante-deux; cinquante+et+un";
+	char str[] = "salut, comment tu VAS ? 42mots quarAnte-deux; cInquante+et+un";
 	printf("%s", ft_strcapitalize(str));
 	return (0);
 }
-*/
+ */
--- c03/ex04/ft_strstr.c
+++ c03/ex04/ft_strstr.c
@@ -6,40 +6,40 @@
 /*   By: niclambe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/12 14:18:50 by niclambe          #+#    #+#             */
-/*   Updated: 2024/08/12 18:52:16 by niclambe         ###   ########.fr       */
+/*   Updated: 2024/08/13 13:08:15 by niclambe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
-#include <string.h>
+#include <string.h> 
 
-char *ft_strstr(char *str, char *to_find)
+char	*ft_strstr(char *str, char *to_find)
 {
-    int i;
-    int j;
+	int	i;
+	int	j;
 
-    i = 0;
-    j = 0;
-    if (to_find[0])
-        return (str);
-    while (str[i] != '\0')
-    {
-        while (str[i + j] == to_find[j])
-        {
-            if (to_find[j + 1] == '\0')
-                return (&str[i]);
-            j++;
-        }
-        j = 0;
-        i++;
-    }
-    return (0);
+	i = 0;
+	j = 0;
+	if (to_find[0] == '\0')
+		return (0);
+	while (str[i] != '\0')
+	{
+		j = 0;
+		while (str[i + j] == to_find[j] && to_find[j] != '\0')
+		{
+			j++;
+		}
+		if (to_find[j] == '\0')
+			return (&str[i]);
+		i++;
+	}
+	return (0);
 }
 
 int main (void)
 {
-    char str[] = "abc";
-    char tofind[] = "z";
-    printf("%s", ft_strstr(str, tofind));
-    return (0);
-}
+	char str[] = "Bonjour je m'apelle";
+	char tofind[] = "jeu";
+	printf("%s", ft_strstr(str, tofind));
+	return (0);
+} 
