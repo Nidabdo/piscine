1394e0da9b02e6b4d4eb5436061580cb024700b7
Nicolas Lambert <niclambe@c1r6p15.42lehavre.fr>
2024-08-19 08:26:35 +0200

    update

--- c05/ex01/ft_recursive_factorial.c
+++ c05/ex01/ft_recursive_factorial.c
@@ -6,7 +6,7 @@
 /*   By: niclambe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/18 18:03:53 by niclambe          #+#    #+#             */
-/*   Updated: 2024/08/18 18:09:19 by niclambe         ###   ########.fr       */
+/*   Updated: 2024/08/19 08:02:20 by niclambe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,5 @@ int ft_recursive_factorial(int nb)
 {
     if (nb == 0 || nb == 1)
         return (1);
-    return (nb * ft_recursive_factorial * (nb - 1));
+    return (nb * ft_recursive_factorial(nb - 1));
 }
-
-int main 
---
+++ c05/ex02/ft_iterative_power.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: niclambe <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/19 08:01:10 by niclambe          #+#    #+#             */
+/*   Updated: 2024/08/19 08:22:17 by niclambe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int ft_iterative_power(int nb, int power)
+{
+    int i;
+    int res;
+
+    res = 1;
+    i = 0;
+    if (nb == 0 && power == 0)
+        return (1);
+    if (power < 0)
+        return (0);
+    else
+    {
+        while (i < power)
+        {
+            res = res * nb;
+            i++;
+        }
+    }
+    return (res);
+}
+// int main(int argc, char *argv[])
+// {
+//     int nb = atoi(argv[1]);
+//     int power = atoi(argv[2]);
+//     if (argc == 3)
+//     {
+//         printf("%d", ft_iterative_power(nb, power));
+//     }
+//     else
+// 		printf("%s", "Error");
+//     return (0);
+// }
