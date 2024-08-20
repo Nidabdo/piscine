cedb482e619b306ac69c3a6b75f616fe9d5228ea
Nicolas Lambert <niclambe@c1r6p15.42lehavre.fr>
2024-08-20 08:21:55 +0200

    update

--- c05/ex06/ft_is_prime.c
+++ c05/ex06/ft_is_prime.c
@@ -6,17 +6,32 @@
 /*   By: niclambe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/19 17:31:32 by niclambe          #+#    #+#             */
-/*   Updated: 2024/08/19 18:32:51 by niclambe         ###   ########.fr       */
+/*   Updated: 2024/08/20 08:20:07 by niclambe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdio.h>
 
 int ft_is_prime(int nb)
 {
     int i;
 
-    i = 0;
-    while (i < nb)
+    i = 2;
+    if (nb <= 1)
+        return (0);
+    while (i * i <= nb)
     {
         if (nb % i == 0)
+            return (0);
+        i++;
     }
+    return (1);
 }
+
+// int main (void)
+// {
+//     int nb = 50;
+
+//     printf("%d", ft_is_prime(nb));
+//     return (0);
+// }
