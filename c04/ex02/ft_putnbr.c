d5471b86fd3d281173281f17d93806d76b738d91
Nicolas Lambert <niclambe@c1r5p1.42lehavre.fr>
2024-08-29 17:32:50 +0200

    update

--- c04/ex02/ft_putnbr.c
+++ c04/ex02/ft_putnbr.c
@@ -11,34 +11,29 @@
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <stdio.h>
 
-void	ft_putchar(char c)
+void ft_putnbr(int nb)
 {
-	write(1, &c, 1);
-}
-
-void	ft_putnbr(int nb)
-{
-	if (nb == -2147483648)
-	{
-		write(1, "-2147483648", 11);
-		return ;
-	}
-	if (nb < 0)
-	{
-		ft_putchar('-');
-		nb = nb * -1;
-	}
-	if (nb >= 10)
-	{
-		ft_putnbr(nb / 10);
-		ft_putnbr(nb % 10);
-	}
-	else
-		ft_putchar(nb + '0');
+    char c;
+    if (nb == -2147483648)
+        write(1, "-2147483648", 11);
+    else
+    {
+        if (nb < 0)
+        {
+            write(1, "-", 1);
+            nb = -nb;
+        }
+        if (nb >= 10)
+            ft_putnbr(nb / 10);
+        c = (nb % 10) + '0';
+        write(1, &c, 1);
+    }
 }
 
 int main(void)
 {
-	ft_putnbr(-2147483648);
+    ft_putnbr(-2147483648);
+    return (0);
 }
---
+++ c04/ex03/atoi_exam.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int	ft_atoi(const char *str)
+{
+    int i;
+    int nb;
+    int sign;
+
+    i = 0;
+    sign = 1;
+    nb = 0;
+
+    while(str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+        i++;
+    if (str[i] == '+' || str[i] == '-')
+    {
+        if(str[i] == '-')
+            sign *= -1;
+        i++;     
+    }
+    while (str[i] >= '0' && str[i] <= '9')
+    {
+        nb = nb * 10 + (str[i] - '0');
+        i++;
+    }
+    return (sign * nb);
+}
+
+int main(void)
+{
+    printf("%i", ft_atoi("    -+80986"));
+    return (0);
+}
